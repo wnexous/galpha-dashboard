e09ebfc468f6a7baf370a9cb70b261803d15ba21
GuKovalski <gugukovalski@hotmail.com>
2023-06-22 12:50:30 -0300

    first commit

---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,10 @@
+import Card from '@/components/card'
+import styles from './styles.module.scss'
+export default function Siginin() {
+    return <div className={styles.teste}>
+        <h1>site</h1>
+        <Card></Card>
+        <Card></Card>
+
+    </div>
+}
